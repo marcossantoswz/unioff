59dbde33dbfd498ab458c1dcb547207b2f67e3dc
Marcos Aurelio Santos <marcos92santos47@gmail.com>
2026-08-26 10:29:45 -0300

    adicionando estrutura de pastas do backend

---
+++ unioff-backend/src/main/java/com/unioff/UnioffApplication.java
@@ -0,0 +1,13 @@
+package com.unioff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnioffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnioffApplication.class, args);
+	}
+
+}
---
+++ unioff-backend/src/test/java/com/unioff/UnioffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unioff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnioffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
