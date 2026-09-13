7a419e44fbd635026993e7fb8d44ce49ba47464a
Leonardo Barreto <leonardobarretogaiao@gmail.com>
2026-09-12 21:53:11 -0300

    feat: Modificação na estrutura da entidade Empresa, endereço foi separado em vários campos

--- unioff-backend/src/main/java/com/unioff/entity/Empresa.java
+++ unioff-backend/src/main/java/com/unioff/entity/Empresa.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
+
 import java.util.List;
 import java.util.UUID;
 
@@ -22,11 +23,27 @@ public class Empresa {
     @Column(name = "nome_fantasia", nullable = false)
     private String nomeFantasia;
 
-    @Column(nullable = false, unique = true)
+    @Column(unique = true)
     private String cnpj;
 
+    private String descricao;
+
     @Column(nullable = false)
-    private String endereco;
+    private String cidade;
+
+    @Column(nullable = false)
+    private String bairro;
+
+    @Column(nullable = false)
+    private String logradouro;
+
+    @Column(nullable = false)
+    private String numero;
+
+    @Column(name = "telephone_whatsapp")
+    private String telephoneWhatsapp;
+
+    private String site;
 
     @OneToOne(fetch = FetchType.LAZY)
     @MapsId
